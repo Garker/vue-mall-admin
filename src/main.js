26ef16d168be660dfbde191093988ce8f4584360
darker <200571164@qq.com>
2021-02-21 17:03:50 +0800

    代码结构优化完成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'ant-design-vue/dist/antd.css';
+import './assets/css/reset.less';
 
 Vue.config.productionTip = false;
 
