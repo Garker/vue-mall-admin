aa75a11ee180852856e63e7151297af4eda824a2
darker <200571164@qq.com>
2021-02-25 16:18:23 +0800

    商品列表页面-商品表格完成

--- src/views/page/productList.vue
+++ src/views/page/productList.vue
@@ -1,29 +1,61 @@
 <template>
   <div>
     <!--  搜索  -->
-    <Search @submit="searchSubmit" :categoryList="categoryList" />
+    <Search @submit="searchSubmit" :categoryList="categoryList" :page="page"/>
+    <!--  表格   -->
+    <products-table :data="tableData" @change="changePage"/>
   </div>
 </template>
 <script>
 import Search from '@/components/search.vue';
+import productsTable from '@/components/productsTable.vue';
 import categoryApi from '@/api/category';
+import api from '@/api/products';
 
 export default {
   components: {
     Search,
+    productsTable,
   },
   data() {
     return {
       categoryList: [],
+      page: {
+        current: 1,
+        pageSize: 10,
+        showSizeChanger: true,
+        total: 1,
+      },
+      tableData: [],
+      categoryObj: [],
     };
   },
   async created() {
     const { data } = await categoryApi.list();
     this.categoryList = data;
+    data.forEach((item) => {
+      this.categoryObj[item.id] = item;
+    });
+    this.getTableData();
   },
   methods: {
     searchSubmit(page) {
       console.log(page);
+    },
+    async getTableData() {
+      const res = await api.list({
+        page: this.page.current,
+        size: this.page.pageSize,
+      });
+      this.page.total = res.total;
+      this.tableData = res.data.map((item) => ({
+        ...item,
+        categoryName: this.categoryObj[item.category].name,
+      }));
+    },
+    changePage(page) {
+      this.page = page;
+      this.getTableData();
     },
   },
 };
